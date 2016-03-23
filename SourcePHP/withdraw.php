e69c7d3652fdd57784f0312c858ccae1c686548a
aravinth_naga <aragetride@gmail.com>
2016-03-24 00:17:24 +0530

    validation

--- SourcePHP/withdraw.php
+++ SourcePHP/withdraw.php
@@ -1,13 +1,16 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="main.css">
+
+
 </head>
 <body class="bg">
 <center>
 <br><br><br><br><br><br><br>
 <br><br><br><br><br><br><br>
 <div id="">
-<form name="withdraw" action='withdrawalProcess.php' method='POST'>
+
+<form name="withdraw"  onsubmit ="allnumeric(NumberOnly)" action='withdrawalProcess.php' method='POST' >
 
 <p> Your Balance is 
 
@@ -40,7 +43,26 @@ echo $_SESSION['firstName'];
 
 <p> Please enter the amount, you wish to withdraw </p>
 
-<input class="input" type="text" name="amount" required>
+<script type="text/javascript">
+function allnumeric(uzip)
+{
+	var number =/^[0-9]+$/;
+	if (uzip.value.match(number))
+	{
+		return true;
+	}
+	
+	else
+	{
+		alert ("vales only");
+	}
+	uzip.focus();
+    return false;
+}
+
+</script>
+
+<input class="input" type="text" name="amount" id="NumberOnly" required>
 
 <input class="input" type="submit" name="submit" value="Withdraw" >
 
